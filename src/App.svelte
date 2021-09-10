f2e160db6c7fc67f9c3812e673c48f3cd82a249d
VincentSastra <vincent.sastra@gmail.com>
2021-09-10 02:23:23 -0700

    Use Poppins font

--- src/App.svelte
+++ src/App.svelte
@@ -21,7 +21,7 @@ import * as data from './data.json'
 <div class="min-h-full min-w-full flex bg-black text-white justify-center pt-20">
 	<div class="flex flex-col justify-center pl-16">
 		<div class="text-6xl mb-8">Hi, it's me</div>
-		<div class="text-9xl mb-8 flex">Vincent<div class="w-8 h-8 bg-orange rounded-full ml-2 mb-2 my-auto"></div></div>
+		<div class="text-9xl mb-8 flex">Vincent<div class="w-8 h-8 bg-orange rounded-full ml-2 mb-4 my-auto"></div></div>
 		<div class="text-6xl mb-4 title-color">A software developer</div>
 		<div class="text-2xl">{data.landingText[0]}</div>
 		<div class="text-2xl">{data.landingText[1]}</div>
@@ -92,7 +92,17 @@ import * as data from './data.json'
 			-webkit-text-fill-color: transparent;
 		}
 	}
+	@font-face {
+		font-family: "Poppins";
+		font-style: normal;
+		font-weight: 400;
+		src: url("https://fonts.googleapis.com/css2?family=Poppins&display=swap");
+		unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6,
+			U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193,
+			U+2212, U+2215, U+FEFF, U+FFFD;
+	}
 	:global(body) {
+		font-family: "Poppins", sans-serif;
 		@apply m-0 p-0 bg-white;
 	}
 </style>
