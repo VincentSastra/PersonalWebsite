801abbbeb618ffb4d653c7a5e32089f99ff063e2
VincentSastra <vincent.sastra@gmail.com>
2021-09-13 23:26:45 -0700

    Fix first few page according to spec

--- src/App.svelte
+++ src/App.svelte
@@ -20,9 +20,9 @@ import * as data from './data.json'
 
 <div class="min-h-full min-w-full flex bg-black text-white justify-center pt-20">
 	<div class="flex flex-col justify-center pl-16">
-		<div class="text-6xl mb-8">Hi, it's me</div>
+		<div class="text-6xl mb-8">hi, it's me</div>
 		<div class="text-9xl mb-8 flex">Vincent<div class="w-8 h-8 bg-orange rounded-full ml-2 mb-4 my-auto"></div></div>
-		<div class="text-6xl mb-4 title-color">A software developer</div>
+		<div class="text-6xl mb-4 title-color">a software developer</div>
 		<div class="text-2xl">{data.landingText[0]}</div>
 		<div class="text-2xl">{data.landingText[1]}</div>
 	</div>
@@ -42,10 +42,10 @@ import * as data from './data.json'
 
 <div class="min-w-full min-h-full flex bg-white text-black">
 	<div class="flex flex-col flex-grow flex-basis-0 skill-bg">
-		<div class="text-6xl mb-10 pl-st pr-52 pt-36">me, myself, and i</div>
+		<div class="text-6xl mb-10 pl-st pr-52 pt-52">me, myself, and i</div>
 		<div class="text-2xl pl-st pr-52">{data.blurb}</div>
 	</div>
-	<div class="flex-grow flex-basis-0 flex justify-center">
+	<div class="flex-grow flex-basis-0 flex justify-center bg-ltorange bg-opacity-30">
 		<div class="grid grid-cols-2 grid-rows-2 my-auto">
 			{#each data.skills as skill }
 				<Skill data={skill} />
@@ -75,7 +75,11 @@ import * as data from './data.json'
 	@tailwind base;
 	@tailwind components;
 	@tailwind utilities;
-
+	@font-face {
+		font-family: "Poppins";
+		src: local('Poppins'), url(../public/Poppins-Regular.ttf) format('ttf');
+	}
+	
 	@layer utilities {
 		.flex-basis-0 {
 			flex-basis: 0;
@@ -99,22 +103,12 @@ import * as data from './data.json'
 			background-image: linear-gradient(132.24deg, rgba(242, 92, 84, 0.8) -9.32%, rgba(244, 132, 95, 0.854167) 35.28%, rgba(247, 157, 101, 0.530744) 77.17%, rgba(247, 178, 103, 0) 120.41%);;
 		}
 		.title-color {
-			background-image: linear-gradient(350deg, #E5E5E5, #F27059);			
+			background: linear-gradient(90.86deg, #F27059 0.33%, #F7B267 46.16%, #FFF4D9 103.85%);
 			-webkit-background-clip: text;
 			-webkit-text-fill-color: transparent;
 		}
 	}
-	@font-face {
-		font-family: "Poppins";
-		font-style: normal;
-		font-weight: 400;
-		src: url("https://fonts.googleapis.com/css2?family=Poppins&display=swap");
-		unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6,
-			U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193,
-			U+2212, U+2215, U+FEFF, U+FFFD;
-	}
 	:global(body) {
-		font-family: "Poppins", sans-serif;
 		@apply m-0 p-0 bg-white;
 	}
-</style>
+	</style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
   purge: ['./public/index.html', './src/**/*.svelte'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'sans': ["Poppins"]
+      }
+    },
   },
   variants: {
     extend: {},
