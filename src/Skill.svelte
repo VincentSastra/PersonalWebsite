6a7ff5d6e0e965fe4e6afc00b5338126a1838870
VincentSastra <vincent.sastra@gmail.com>
2021-09-25 17:47:09 -0700

    Adjust Content and icons

--- src/Skill.svelte
+++ src/Skill.svelte
@@ -2,9 +2,19 @@
 	export let data
 </script>
 
-<div class="my-6">
-	<div class="text-4xl font-bold mb-2">{data.title}</div>
+<div class="my-6 p-6">
+	<div class="flex justify-start">
+		<i class={data.icon} />
+		<div class="text-4xl font-bold mb-2">{data.title}</div>
+	</div>
 	{#each data.list as item}
 		<div class="text-4xl ml-2">└ <span class="text-2xl">{item}</span></div>
 	{/each}
 </div>
+
+<style>
+	i {
+		margin: auto 15px;
+		font-size: xx-large;
+	}
+</style>
